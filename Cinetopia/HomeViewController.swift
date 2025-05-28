9304b0be13824ef5b44a958ead634929d2af8af3
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-05-28 17:18:53 -0300

    Configurações Iniciais
    Adicionando ícone do app, imagens e cores que vão ser utilizadas no projeto.
    Configurando projeto para trabalhar com viewcode.

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Cinetopia
 //
 //  Created by Juliano Sgarbossa on 28/05/25.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
     }
 
 
